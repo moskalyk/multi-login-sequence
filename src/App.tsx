991a400bc1422a8a148796f43965bdfe6159690f
moskalyk <morgan.moskalyk@protonmail.ch>
2023-03-24 15:36:40 -0400

    added code for test

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {sequence} from '0xsequence'
 
 function App() {
+  React.useEffect(() => {
+    sequence.initWallet('mumbai')
+
+    const login = async (networkId: any) => {
+        const wallet = await sequence.getWallet()
+        const connectDetails = await wallet.connect({
+          app: 'multi',
+          authorize: true,
+          // And pass settings if you would like to customize further
+          settings: {
+            theme: "purple",
+          },
+          networkId: networkId
+        })
+        console.log(connectDetails)
+      }
+      login(80001)
+      setTimeout(async () => {
+        login(137)
+      }, 4000)
+    })
   return (
     <div className="App">
       <header className="App-header">
